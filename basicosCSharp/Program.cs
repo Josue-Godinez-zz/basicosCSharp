c312f0826bbcdbe6cb396d4dbd1ef24ec820160f
ShuAkihiro <josuegod1998@gmail.com>
2019-11-30 09:00:26 -0600

    Se inicializa el proyeto

---
+++ basicosCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace basicosCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
