15310d5b4fe6dd6acc1fd07c0f0e2c7a241ddc12
ShuAkihiro <josuegod1998@gmail.com>
2019-11-30 09:22:52 -0600

    Inicializacion de Variables

--- basicosCSharp/Program.cs
+++ basicosCSharp/Program.cs
@@ -6,7 +6,18 @@ namespace basicosCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int añoActual = Convert.ToInt32("2019");
+            double numeroPi = 3.141592;
+            Console.WriteLine("Valores:{0} -- {1}", añoActual, numeroPi);
+            decimal montoFactura = 94593.34m + 0.07m;
+            float impuestoVenta = 9239.04f;
+            Console.WriteLine("Valores:{0} -- {1}", montoFactura, impuestoVenta);
+            char letraInicial = 'P';
+            string nombreCurso = letraInicial + "rogramacion II";
+            Console.WriteLine(nombreCurso);
+            bool boolVerdadero = true;
+            bool boolFalso = false;
+            Console.WriteLine("Variables boleanas: {0} y {1}", boolVerdadero, boolFalso);
         }
     }
 }
