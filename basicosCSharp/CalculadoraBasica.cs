2df1f4cab7c97e670ff4d808191b56e50f02fbfd
ShuAkihiro <josuegod1998@gmail.com>
2019-11-30 10:51:25 -0600

    Creacion de clase

---
+++ basicosCSharp/CalculadoraBasica.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class CalculadoraBasica
+{
+
+    private double ultimoResultado;
+    public CalculadoraBasica()
+    {
+    }
+
+    public double Sumar(double primerValor, double segundoValor)
+    {
+        ultimoResultado = primerValor + segundoValor;
+        return ultimoResultado;
+    }
+    public double Dividir(double primerValor, double segundoValor)
+    {
+        return primerValor / segundoValor;
+    }
+}
